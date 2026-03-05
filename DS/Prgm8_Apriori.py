a46c1d16384ea7a0fda853c0d468041254dd8b93
Prabhava_UV <prabhava.uv@gmail.com>
2026-03-05 15:42:05 +0530

    Fourth Push

--- DS/Prgm8_Apriori.py
+++ DS/Prgm8_Apriori.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from mlxtend.frequent_patterns import apriori, association_rules
-df=pd.read_csv("Data/Market_Basket_Optimisation.csv")
+df=pd.read_csv("DS/Data/market_basket.csv")
 print(df.columns)
+
 df.isna().sum()
-df.fillna(inplace=True)
-item=apriori(df,min_support=0.01,use_colnames=True)
-print(item)
-rules=association_rules(item,metric="confidence",min_threshold=0.1)
-rules.sort(['support','confidence'],ascending=False)
+df.fillna('', inplace=True)
+df_dum = pd.get_dummies(df)
+items = apriori(df_dum,min_support=0.01, use_colnames=True)
+print(items)
+
+rules=association_rules(items,metric="confidence",min_threshold=0.1)
+rules.sort_values(['support','confidence'],ascending=False)
 print(rules)
--- DS/Prgm9_KMeans.py
+++ DS/Prgm9_KMeans.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df=pd.read_csv("Data/Mall_Customers.csv")
+df=pd.read_csv("DS/Data/Mall_Customers.csv")
 print(df.columns)
 
 le=LabelEncoder()
@@ -13,13 +13,13 @@ sc=StandardScaler()
 df["Genre"]=le.fit_transform(df["Genre"])
 X=df[["Age","Genre","Annual Income (k$)","Spending Score (1-100)"]]
 
-X=sc.fit_transform(X)
+x_scaled=sc.fit_transform(X)
 
 iner=[]
 
 for i in range(1,11):
     km=KMeans(n_clusters=i,random_state=42,n_init=10)
-    km.fit(X)
+    km.fit(x_scaled)
     iner.append(km.inertia_)
 
 plt.figure(figsize=(10,6))
@@ -29,14 +29,16 @@ plt.ylabel("Inertia")
 plt.title("Elbow Method for Optimal K")
 plt.show()
 
-km=KMeans(n_clusters=5,random_state=42)
-cl=km.predict(X)
 
-plt.figure(figsize=(10,6))
-sns.scatterplot(x=df["Annual Income (k$)"],y=df["Spending Score (1-100)"], hue=cl, palette="viridis", s=100)
-plt.scatter(km.cluster_centers_[:,2],km.cluster_centers_[:,3],s=200,c="red",marker="x", label="Centroids")
-plt.xlabel("Annual Income (k$)")
-plt.ylabel("Spending Score (1-100)")
-plt.title("KMeans Cluster")
-plt.legend("Clusters")
+kmeans = KMeans(n_clusters=5, n_init=10, random_state=42)
+data = kmeans.fit_predict(X)  
+
+plt.figure(figsize=(8, 6))
+sns.scatterplot(x=df['Annual Income (k$)'], y=df['Spending Score (1-100)'], hue=data, palette='viridis', s=100)
+plt.scatter(kmeans.cluster_centers_[:, 2], kmeans.cluster_centers_[:, 3], s=300, c='red', marker='x', label='Centroids')
+
+plt.title('K-Means Clustering')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend(title='Cluster')
 plt.show()
